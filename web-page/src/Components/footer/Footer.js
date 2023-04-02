38730dea5bf70cdeb90cf4764bb3e3c779f85c50
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-04-02 20:44:44 +0000

    done 70%

---
+++ web-page/src/Components/footer/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <>
+            <a href="https://github.com/Ahmad-Alanati">social media</a>
+            <h1>ahmad jehad alanati</h1>
+        </>
+    );
+}
+
+export default Footer;
---
+++ web-page/src/Components/tours/Tours.js
@@ -0,0 +1,8 @@
+function Tours() {
+    let data = require('../../Data/db.json');
+    return (
+        <h1>this is the Tours</h1>
+    );
+  }
+
+  export default Tours;
